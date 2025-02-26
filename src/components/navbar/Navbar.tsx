3e15ddd508e4de5fc26638e4f4750f843e10a4ad
Tainá Lara <tainalara.dev@gmail.com>
2025-02-26 12:16:07 -0300

    Home, navbar e footer

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+
+function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-6
+            			  bg-[#849cc1] text-white'>
+            
+                <div className="container flex justify-between text-lg">
+                <Link to='/home' className="text-2xl font-bold">App Carona Legal</Link>
+
+                    <div className='flex gap-4'>
+                    Motorista
+                    Usuário
+                    Sobre                    
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
